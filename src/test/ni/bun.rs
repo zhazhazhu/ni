55308c51c26a9661219760d94d049139849e4a08
zhazhazhu <fangyuan.leslie@qq.com>
2023-12-21 00:23:25 +0800

    feat: add ni test for bun

---
+++ src/test/ni/bun.rs
@@ -0,0 +1,45 @@
+#[cfg(test)]
+mod bun {
+    use crate::agents::*;
+    use crate::parse::*;
+
+    #[test]
+    fn empty() {
+        let (agent, args) = parse_ni(Agent::Bun, Vec::new());
+        assert_eq!(agent, "bun");
+        assert_eq!(args, ["install"]);
+    }
+    #[test]
+    fn single_add() {
+        let (agent, args) = parse_ni(Agent::Bun, vec!["axios".to_string()]);
+        assert_eq!(agent, "bun");
+        assert_eq!(args, ["add", "axios"]);
+    }
+    #[test]
+    fn multiple() {
+        let (agent, args) = parse_ni(Agent::Bun, vec!["axios".to_string(), "eslint".to_string()]);
+        assert_eq!(agent, "bun");
+        assert_eq!(args, ["add", "axios", "eslint"]);
+    }
+    #[test]
+    fn dependencies() {
+        let (agent, args) = parse_ni(
+            Agent::Bun,
+            vec!["axios".to_string(), "eslint".to_string(), "-D".to_string()],
+        );
+        assert_eq!(agent, "bun");
+        assert_eq!(args, ["add", "axios", "eslint", "-d"]);
+    }
+    #[test]
+    fn global() {
+        let (agent, args) = parse_ni(Agent::Bun, vec!["axios".to_string(), "-g".to_string()]);
+        assert_eq!(agent, "bun");
+        assert_eq!(args, ["add", "-g", "axios"]);
+    }
+    #[test]
+    fn frozen() {
+        let (agent, args) = parse_ni(Agent::Bun, vec!["--frozen".to_string()]);
+        assert_eq!(agent, "bun");
+        assert_eq!(args, ["install", "--no-save"]);
+    }
+}
