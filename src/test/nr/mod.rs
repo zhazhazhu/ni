d00477f9be445d8548cd9e906f84589c1494945c
zhazhazhu <fangyuan.leslie@qq.com>
2023-12-27 02:52:29 +0800

    test: add yarn_berry

--- src/test/nr/mod.rs
+++ src/test/nr/mod.rs
@@ -2,3 +2,4 @@ mod bun;
 mod npm;
 mod pnpm;
 mod yarn;
+mod yarn_berry;
---
+++ src/test/nr/yarn_berry.rs
@@ -0,0 +1,39 @@
+#[cfg(test)]
+mod bun {
+    use crate::agents::*;
+    use crate::parse::*;
+
+    #[test]
+    fn empty() {
+        let (agent, args) = parse_nr(Agent::YarnBerry, Vec::new());
+        assert_eq!(agent, "yarn");
+        assert_eq!(args, ["run", "start"]);
+    }
+    #[test]
+    fn if_present() {
+        let (agent, args) = parse_nr(Agent::YarnBerry, vec!["test".into(), "--if-present".into()]);
+        assert_eq!(agent, "yarn");
+        assert_eq!(args, ["run", "--if-present", "test"]);
+    }
+    #[test]
+    fn script() {
+        let (agent, args) = parse_nr(Agent::YarnBerry, vec!["dev".into()]);
+        assert_eq!(agent, "yarn");
+        assert_eq!(args, ["run", "dev"]);
+    }
+    #[test]
+    fn script_with_arguments() {
+        let (agent, args) = parse_nr(
+            Agent::YarnBerry,
+            vec!["build".into(), "--watch".into(), "-o".into()],
+        );
+        assert_eq!(agent, "yarn");
+        assert_eq!(args, ["run", "build", "--watch", "-o"]);
+    }
+    #[test]
+    fn colon() {
+        let (agent, args) = parse_nr(Agent::YarnBerry, vec!["build:dev".into()]);
+        assert_eq!(agent, "yarn");
+        assert_eq!(args, ["run", "build:dev"]);
+    }
+}
