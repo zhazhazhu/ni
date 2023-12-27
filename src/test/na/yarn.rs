c0353276984e790d2ae025041955c3649abf5317
zhazhazhu <fangyuan.leslie@qq.com>
2023-12-27 14:25:44 +0800

    test: add yarn na

---
+++ src/test/na/yarn.rs
@@ -0,0 +1,24 @@
+#[cfg(test)]
+mod bun {
+    use crate::agents::*;
+    use crate::parse::*;
+
+    #[test]
+    fn empty() {
+        let (agent, args) = parse_na(Agent::Yarn, vec![], None);
+        assert_eq!(agent, "yarn");
+        assert_eq!(args.len(), 0);
+    }
+    #[test]
+    fn foo() {
+        let (agent, args) = parse_na(Agent::Yarn, vec!["foo".into()], None);
+        assert_eq!(agent, "yarn");
+        assert_eq!(args, ["foo"]);
+    }
+    #[test]
+    fn run_test() {
+        let (agent, args) = parse_na(Agent::Yarn, vec!["run".into(), "test".into()], None);
+        assert_eq!(agent, "yarn");
+        assert_eq!(args, ["run", "test"]);
+    }
+}
