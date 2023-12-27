f9e8493037dcd1f4f8b1e9c694222119df019dbc
zhazhazhu <fangyuan.leslie@qq.com>
2023-12-27 14:02:19 +0800

    test: add npm nlx

--- src/test/nlx/mod.rs
+++ src/test/nlx/mod.rs
@@ -1,1 +1,2 @@
 mod bun;
+mod npm;
---
+++ src/test/nlx/npm.rs
@@ -0,0 +1,18 @@
+#[cfg(test)]
+mod bun {
+    use crate::agents::*;
+    use crate::parse::*;
+
+    #[test]
+    fn single_uninstall() {
+        let (agent, args) = parse_nlx(Agent::Npm, vec!["esbuild".into()], None);
+        assert_eq!(agent, "npx");
+        assert_eq!(args, ["esbuild"]);
+    }
+    #[test]
+    fn multiple() {
+        let (agent, args) = parse_nlx(Agent::Npm, vec!["esbuild".into(), "--version".into()], None);
+        assert_eq!(agent, "npx");
+        assert_eq!(args, ["esbuild", "--version"]);
+    }
+}
